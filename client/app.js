9f2c0a1b4e895abafd84b44bf3974b98c1a12b40
cianclarke <cian@cianclarke.com>
2011-10-26 16:32:45 +0100

    boilerplate hello world app using MVC

---
+++ client/app.js
@@ -0,0 +1,7 @@
+Ext.regApplication({
+    name: 'Presencha',
+    launch: function() {
+      this.views.viewport = new this.views.Viewport();
+
+    }
+});
---
+++ client/views/Viewport.js
@@ -0,0 +1,23 @@
+Presencha.views.Viewport = Ext.extend(Ext.Panel, {
+    fullscreen: true,
+    layout: 'card',
+    cardSwitchAnimation: 'slide',
+    initComponent: function() {
+        //put instances of cards into app.views namespace
+        Ext.apply(Presencha.views, {
+            //contactsList: new app.views.ContactsList(),
+            //contactDetail: new app.views.ContactDetail(),
+            //contactForm: new app.views.ContactForm()
+        });
+        //put instances of cards into viewport
+        Ext.apply(this, {
+            items: [
+                {
+                  xtype: 'panel',
+                  html: 'hello world'
+                }
+            ]
+        });
+        Presencha.views.Viewport.superclass.initComponent.apply(this, arguments);
+    }
+});
