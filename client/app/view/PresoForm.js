23bd25796ad4e36c731345fd4dd870e518830ec5
Drew Neil <andrew.jr.neil@gmail.com>
2011-10-26 13:30:45 -0500

    Add the form + button back into PresoForm

--- client/app/view/PresoForm.js
+++ client/app/view/PresoForm.js
@@ -2,6 +2,46 @@ Ext.define('Presencha.view.PresoForm', {
     extend: 'Ext.Panel',
     xtype: 'presoform',
     config: {
-        html: 'asdf'
+        layout: {
+            type: 'vbox',
+            pack: 'center'
+        },
+        
+        items: [
+            {
+                xtype: 'toolbar',
+                title: 'Upload your slideshow',
+                docked: 'top'
+            },
+            {
+                html: [
+                  '<form id="slideshow"',
+                  ' action="http://api.presencha.com/slideshow"',
+                  ' enctype="multipart/form-data"',
+                  ' method="POST">',
+                  '<fieldset>',
+                  '  <label>Title:',
+                  '    <input type="text" name="title">',
+                  '  </label><br/>',
+                  '  <label>PDF:',
+                  '    <input type="file" name="slides">',
+                  '  </label>',
+                  '</fieldset>',
+                  '</form>',
+                ].join(" ")
+            },
+            {
+                xtype: 'toolbar',
+                docked: 'bottom',
+                items: [
+                    { xtype: 'spacer' },
+                    {
+                        text: 'submit',
+                        ui: 'confirm'
+                    },
+                    { xtype: 'spacer' }
+                ] 
+            }
+        ]
     }
 });
