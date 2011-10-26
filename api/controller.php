ec3e0ca68aac3f456e925e233a23dabc34db9df8
Martin de Keijzer <martin.dekeijzer@gmail.com>
2011-10-26 19:31:10 +0200

    fixed htaccess

--- api/controller.php
+++ api/controller.php
@@ -1,4 +1,7 @@
 <?php
+// A little config
+$slideshowFolder = dirname(__FILE__) . '/slides';
+
 /** a damn wrong and filthy implementation of a controller, just for hacking purposes */
 header('Content-type: application/json');
 
@@ -32,19 +35,20 @@ switch ($_GET['mode']) {
             );
             echo json_encode($response);
         } else {
-            $slideshow = array();
-            // loop through directories in ./slides
-        //    $path = 'slides';
-        //    $results = scandir($path);
-        //
-        //    foreach ($results as $result) {
-        //        if ($result === '.' or $result === '..') continue;
-        //
-        //        if (is_dir($path . '/' . $result)) {
-        //            //code to use if directory
-        //        }
-        //    }
-            echo json_encode($slideshow);
+            header('Content-type: text/html');
+            ?><!doctype html>
+                <html>
+                    <head>
+                        <title>Upload a slideshow</title>
+                    </head>
+                    <body>
+                        <form method="post" enctype="application/x-www-form-urlencoded">
+                            <input type="file" name="slideshow">
+                            <input type="submit" name="Upload">
+                        </form>
+                    </body>
+                </html>
+            <?
         }
         break;
 }
