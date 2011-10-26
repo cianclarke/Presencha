5dadc8537e4819cd171fb0d9e67479919a204487
cianclarke <cian@cianclarke.com>
2011-10-26 17:11:32 +0100

    Add models for Slide and Slideshow

---
+++ client/models/Slide.js
@@ -0,0 +1,7 @@
+Presencha.models.Slide = Ext.regModel("Presencha.models.Slide", {
+    fields: [
+        {name: "image", type: "string"}
+        // order? probably redundant?
+        // audio timestamp notes? 
+    ]
+});
