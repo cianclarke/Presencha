137740fbf49011ad3614389706089d4bddc35213
Drew Neil <andrew.jr.neil@gmail.com>
2011-10-26 12:49:13 -0500

    Create a PresoForm widget, with HTML form.

--- client/views/PresoForm.js
+++ client/views/PresoForm.js
@@ -1,7 +1,49 @@
 Presencha.views.PresoForm = Ext.extend(Ext.Panel, {
-  html: 'Upload yer PDF here.',
-  
+
   initComponent: function() {
+    Ext.apply(this, {
+      layout: {
+        type: 'vbox',
+        pack: 'center'
+      },
+      dockedItems: [
+        {
+          xtype: 'toolbar',
+          title: 'Upload your slideshow'
+        },
+        {
+          xtype: 'toolbar',
+          dock: 'bottom',
+          items: [
+            { xtype: 'spacer' },
+            {
+              text: 'submit',
+              ui: 'confirm'
+            },
+            { xtype: 'spacer' }
+          ]
+        }
+      ],
+      items: [
+        {
+          html: [
+            '<form id="slideshow"',
+            ' action="http://api.presencha.com/slideshow"',
+            ' enctype="multipart/form-data"',
+            ' method="POST">',
+            '<fieldset>',
+            '  <label>Title:',
+            '    <input type="text" name="title">',
+            '  </label><br/>',
+            '  <label>PDF:',
+            '    <input type="file" name="slides">',
+            '  </label>',
+            '</fieldset>',
+            '</form>',
+          ]
+        }
+      ]
+    })
     Presencha.views.PresoForm.superclass.initComponent.apply(this, arguments);
   }
 });
